72f3cac611c4cdad4118f782734834feaf77201d
Kelii DeLeon <keliikeideleon@gmail.com>
2023-08-29 12:35:04 -0600

    Working serial and word finder

--- serial.py
+++ serial.py
@@ -1,7 +1,7 @@
 """Python serial number generator."""
 
 class SerialGenerator:
-    """Machine to create unique incrementing serial numbers.
+    """Class to create unique incrementing serial numbers.
     
     >>> serial = SerialGenerator(start=100)
 
@@ -19,4 +19,24 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__ (self, start=0):
+        """Make a new serial generator with starting serial number"""
+        self.start = start
+        self.current_serial = start
+
+    def __repr__(self):
+        """Show representation."""
+
+        return f"<SerialGenerator start={self.start} current_serial={self.current_serial}>"
+    
+    def generate(self):
+        """Return next serial number"""
+        serial = self.current_serial
+        self.current_serial += 1
+        return serial
+    
+    def reset(self):
+        """Reset serial number back to starting serial number"""
+        self.current_serial = self.start
+    
 
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,40 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    """Class to read in and find random words from dictionary
+    
+    >>> wf = WordFinder("words.txt")
+    235888 words read
+
+    >>> wf.getRandomWord() in wf.words
+    True
+    
+    """
+    def __init__(self, path):
+        """Reads in words and ouputs number of words read"""
+        file = open(path)
+        self.words = self.parseWords(file)
+        print(f"{len(self.words)} words read")
+
+    def parseWords(self, file):
+        return [word.strip() for word in file]
+
+    def getRandomWord(self):
+        return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    """Specialized WordFinder that removes comments
+    
+    >>> swf = SpecialWordFinder("./words.txt")
+    235886 words read
+
+    >>> swf.getRandomWord() in swf.words
+    True
+    """
+
+    def parseWords(self, file):
+        """Parse file -> list of words, skipping blanks/comments."""
+
+        return [word.strip() for word in file
+                if word.strip() and not word.startswith("#")]
